e89d793c85621c1c9ff392e3efb48726701546a2
kumiho <konstantin.kim.mail@gmail.com>
2017-12-14 00:14:01 +0300

    change limits and update todo

--- src/LiveServer.Client/Program.cs
+++ src/LiveServer.Client/Program.cs
@@ -8,7 +8,7 @@ namespace LiveServer.Client
 {
     internal class Program
     {
-        private const int Items = 10_000;
+        private const int Items = 100;
         private static readonly Random Random = new Random();
 
         private static void Main()
